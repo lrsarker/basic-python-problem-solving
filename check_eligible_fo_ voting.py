ea9e7e81612100c02e1d5cd417b2a96f6f6adf37
Lutfur Rahman Sarker <eng.lrsarker@gmail.com>
2023-03-19 12:27:54 +0600

    Update check_eligible_fo_ voting.py

--- check_eligible_fo_ voting.py
+++ check_eligible_fo_ voting.py
@@ -1,3 +1,4 @@
+# Write a Python Program to check whether a Person is eligible for Voting!
 age = float(input("Enter your Age: "))
 if age >= 18:
     print("Congratulations! your are eligible to Vote")
