ad4d988e30a37156ed60644f0a10c7e761551356
Lutfur Rahman Sarker <eng.lrsarker@gmail.com>
2023-03-20 14:05:19 +0600

    Add files via upload
    Pyramid Triangle using python programming (Basic loop)

---
+++ pyramid_triangle_pattern.py
@@ -0,0 +1,15 @@
+#Python Pyramid/Triangle Pattern with loop.
+i = 0
+while i <= 6:
+    print(i * '*')
+    i = i + 1
+print("Reverse")
+i = 6
+while i >= 0:
+    print(i * '*')
+    i = i - 1
+
+
+#Apply for loop.
+for i in range(7):
+    print(i * '*')
